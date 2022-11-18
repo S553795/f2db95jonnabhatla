9a0a7df2e9afbaf8a4d74489dae8ce00e378b16c
satya <satyajonnabhatla98@gmail.com>
2022-11-18 08:47:40 -0600

    added passport security

--- app.js
+++ app.js
@@ -54,6 +54,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -63,7 +70,14 @@ app.use('/gridbuild', gridBuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
-
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
